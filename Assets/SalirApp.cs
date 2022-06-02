36c467d7e5fc07fd721d85d036a785a9139313dd
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-02 10:57:12 -0300

    scenes

---
+++ Assets/SalirApp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalirApp : MonoBehaviour
+{
+    public void salirJuego()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
