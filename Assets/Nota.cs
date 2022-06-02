1ecc37a17672d3c0f3ea3854095f9e57e69def5a
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-02 15:54:42 -0300

    notitas

---
+++ Assets/Nota.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nota : MonoBehaviour
+{
+    public GameObject ObjPuntos;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ObjPuntos.GetComponent<Puntos>().puntos += 1;
+            Destroy(gameObject);
+        }
+    }//minuto 5:50 https://www.youtube.com/watch?v=9A-kE0Y_R_k
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
