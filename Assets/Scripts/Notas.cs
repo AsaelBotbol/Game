4cfd0a6e1e81fa4b43179757a31c0086cc6b5f26
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-03 20:38:41 -0300

    intento de solucion xd

--- Assets/Scripts/Notas.cs
+++ Assets/Scripts/Notas.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class Notas : MonoBehaviour
 {
     public GameObject ObjPuntos;
+    public AudioClip papersound;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag=="Player")
         {
             ObjPuntos.GetComponent<Puntos>().puntos += 1;
+
+            audioNota.clip = papersound;
+            audioNota.Play();
+
             Destroy(gameObject);
         }
     }
+    AudioSource audioNota;
 
     // Start is called before the first frame update
     void Start()
     {
         
+        audioNota = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
