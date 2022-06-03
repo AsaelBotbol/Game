613ab6e884c448477220ff999db705b4edaba04b
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-03 13:40:08 -0300

    elimine assets que no use

---
+++ Assets/Scripts/Notas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notas : MonoBehaviour
+{
+    public GameObject ObjPuntos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            ObjPuntos.GetComponent<Puntos>().puntos += 1;
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
