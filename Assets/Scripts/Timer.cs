3029c2e59b50bdecfadd09fdd3b37b6f52297c6a
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-08 08:52:29 -0300

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 0;
+
+    public Text textoTimer;
+
+    public bool existe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        textoTimer.text = "" + timer.ToString();
+
+        if (timer < 0)
+        {
+            textoTimer.text = "El bicho ha sido liberado.";
+
+            Destroy(textoTimer, 6);
+        }
+
+    }
+}
